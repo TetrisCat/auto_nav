01ca8876f0e5ab04e9d3bc098f1ec5894f7114a7
Cyril Aoun <cyril_aoun99@outlook.com>
2020-02-03 12:30:47 +0800

    Translation Transformation

--- occupancy2.py
+++ occupancy2.py
@@ -30,8 +30,9 @@ def callback(msg, tfBuffer):
 
     # find transform to convert map coordinates to base_link coordinates
     # lookup_transform(target_frame, source_frame, time)
+    
     trans = tfBuffer.lookup_transform('base_link', 'map', rospy.Time(0))
-    rospy.loginfo(['Trans: ' + str(trans.transform.translation)])
+    rospy.loginfo(['Trans: ' + str(trans.transform.translation.x)])
     rospy.loginfo(['Rot: ' + str(trans.transform.rotation)])
     # convert quaternion to Euler angles
     orientation_list = [trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w]
@@ -39,7 +40,7 @@ def callback(msg, tfBuffer):
     rospy.loginfo(['Yaw: R: ' + str(yaw) + ' D: ' + str(np.degrees(yaw))])
 
     # make occdata go from 0 instead of -1, reshape into 2D
-    oc2 = occdata + 1
+    oc2 = occdata[0] + 1
     # set all values above 1 (i.e. above 0 in the original map data, representing occupied locations)
     oc3 = (oc2>1).choose(oc2,2)
     # reshape to 2D array using column order
@@ -48,16 +49,23 @@ def callback(msg, tfBuffer):
     img = Image.fromarray(odata)
     # rotate by 180 degrees to invert map so that the forward direction is at the top of the image
     rotated = img.rotate(np.degrees(yaw)+180)
-    # show image using grayscale map
-    plt.imshow(rotated,cmap='gray')
+    x_trans = -trans.transform.translation.x / msg.info.resolution
+    y_trans = -trans.transform.translation.y / msg.info.resolution
+
+    translatedaffine = np.array([[1, 0, y_trans],[0, 1, x_trans],[0,0,1]])
+    T_inv = np.linalg.inv(translatedaffine)
+    
+    translated = rotated.transform((msg.info.width,msg.info.height),Image.AFFINE,data=T_inv.flatten()[:6])
+    # show image using grayscale ma
+    plt.imshow(translated,cmap ='gray')
     plt.draw_all()
     # pause to make sure the plot gets created
     plt.pause(0.00000000001)
 
 
-def occupancy():
+def occupancy2():
     # initialize node
-    rospy.init_node('occupancy', anonymous=True)
+    rospy.init_node('occupancy2', anonymous=True)
 
     tfBuffer = tf2_ros.Buffer()
     tfListener = tf2_ros.TransformListener(tfBuffer)
@@ -67,6 +75,8 @@ def occupancy():
     rospy.Subscriber('map', OccupancyGrid, callback, tfBuffer)
 
     plt.ion()
+#    plt.rc('grid',linestyle = "-", color = "white")
+#    plt.grid(True)
     plt.show()
 
     # spin() simply keeps python from exiting until this node is stopped
@@ -75,6 +85,6 @@ def occupancy():
 
 if __name__ == '__main__':
     try:
-        occupancy()
+        occupancy2()
     except  rospy.ROSInterruptException:
         pass
